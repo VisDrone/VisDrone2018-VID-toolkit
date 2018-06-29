0c50e50e3179a98e9b34dc54489a417281abab71
VisDrone2018 <38520822+VisDrone2018@users.noreply.github.com>
2018-06-29 15:51:05 +0800

    Update saveAnnoRes.m

--- saveAnnoRes.m
+++ saveAnnoRes.m
@@ -1,21 +1,18 @@
-function [allgt, alldet] = saveAnnoRes(gtPath, resPath, seqPath, numSeqs, nameSeqs, isNMS, nmsThre)
+function [allgt, alldet] = saveAnnoRes(gtPath, resPath, seqPath, numSeqs, nameSeqs)
 %% process the annotations and groundtruth
 allgt = cell(1,numSeqs);
 alldet = cell(1,numSeqs);
 
 for idSeq = 1:numSeqs
     oldgt = load(fullfile(gtPath, nameSeqs{idSeq}));
-    det = load(fullfile(resPath, nameSeqs{idSeq}));
+    olddet = load(fullfile(resPath, nameSeqs{idSeq}));
     % remove the objects in ignored regions or labeled as others
     img = imread(fullfile(seqPath, nameSeqs{idSeq}(1:end-4), '0000001.jpg'));
     [imgHeight, imgWidth, ~] = size(img);     
-    newgt = dropObjectsInIgr(oldgt, imgHeight, imgWidth);    
+    [newgt, newdet] = dropObjectsInIgr(oldgt, olddet, imgHeight, imgWidth);    
     gt = newgt;
     gt(newgt(:,7) == 0, 7) = 1;
     gt(newgt(:,7) == 1, 7) = 0; 
-    % NMS
-    newdet = nmsProcess(det, isNMS, nmsThre);
-    [~, id] = sort(-newdet(:,7));
     allgt{idSeq} = gt;
-    alldet{idSeq} = newdet(id,:);
+    alldet{idSeq} = newdet;
 end
